b4e44f658bf5260b38eaebcd2f46717268b77554
Arturo Carretero Calvo <10163049+ArtCC@users.noreply.github.com>
2025-06-28 16:25:06 +0200

    Remove redundant tests and imports from various test files
    - Remove all test functions from CommentRequestTests.swift
    - Remove all test functions from CommentTests.swift
    - Remove all test functions from ResponseDecodingTests.swift
    - Remove all test functions from SuggestionRequestTests.swift
    - Remove all test functions from SuggestionTests.swift
    - Remove all test functions from TypeAliasTests.swift
    - Remove all test functions from VoteTests.swift
    - Remove all test functions from VoteTypeTests.swift
    - Remove all test functions from VoticeErrorTests.swift
    - Remove import statements for Testing and Foundation from all modified test files

--- Tests/VoticeTests/Models/VoteTypeTests.swift
+++ Tests/VoticeTests/Models/VoteTypeTests.swift
@@ -5,32 +5,3 @@
 //  Created by Arturo Carretero Calvo on 28/6/25.
 //  Copyright © 2025 ArtCC. All rights reserved.
 //
-
-import Testing
-@testable import Votice
-import Foundation
-
-// MARK: - Suggestion Request Tests
-
-@Test("VoteType should encode and decode correctly")
-func testVoteTypeEncodingDecoding() async throws {
-    // Given
-    let voteTypes: [VoteType] = [.upvote, .downvote, .remove]
-
-    for voteType in voteTypes {
-        // When
-        let encodedData = try JSONEncoder().encode(voteType)
-        let decodedVoteType = try JSONDecoder().decode(VoteType.self, from: encodedData)
-
-        // Then
-        #expect(decodedVoteType == voteType)
-    }
-}
-
-@Test("VoteType raw values should be correct")
-func testVoteTypeRawValues() async {
-    // Then
-    #expect(VoteType.upvote.rawValue == "upvote")
-    #expect(VoteType.downvote.rawValue == "downvote")
-    #expect(VoteType.remove.rawValue == "remove")
-}
